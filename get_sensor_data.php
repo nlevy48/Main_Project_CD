ef425c5fe0a72ff8f23ba2258ce9d691db87d1e3
nlevy48 <nlevy@friendsbalt.org>
2024-12-08 16:44:03 -0500

    Added code to sql

--- get_sensor_data.php
+++ get_sensor_data.php
@@ -1,1 +1,6 @@
 // receives data from arduion oand saves it to sql
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sensor_database"
---
+++ sensor_database.sql
@@ -0,0 +1,8 @@
+CREATE Table sensor_data (
+    SensorID INT PRIMARY KEY,
+    SensorName VARCHAR(50)
+    SensotValue FLOAT
+    Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+
+INSERT INTO sensor_data (SensorID, SensorName, SensorValue)
+)
