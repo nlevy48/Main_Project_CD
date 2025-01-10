8ed7b8715e1a503d0b0e79aa1f6e6d57facc626e
nlevy48 <nlevy@friendsbalt.org>
2025-01-10 13:45:18 -0500

    got the random number to change dynamically

---
+++ send_random_number_data.php
@@ -0,0 +1,30 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "noahlevy";
+$password = "angus999";
+$dbname = "RandomNumberDatabase";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query to retrieve data
+$sql = "SELECT value FROM RandomNumbers ORDER BY created_at DESC LIMIT 1";
+$result = $conn->query($sql);
+
+$conn->close();
+
+$json = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $json[] = $row;
+    }
+}
+
+json_encode($json);
+?>
