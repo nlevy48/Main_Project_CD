ea1442d4019b61ded12089de31a94343762c7690
nlevy48 <nlevy@friendsbalt.org>
2025-01-21 10:13:03 -0500

    got main code up and running, but 500 error occured

--- get_sensor_data.php
+++ get_sensor_data.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 // receives data from arduino oand saves it to sql
 
 $servername = "localhost";
@@ -10,12 +13,23 @@ $dbname = "SepticTankEnvironmentalConditions";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    http_response_code(500);
+    echo json_encode(["error" => "Connection failed: " . $conn->connect_error]);
+    exit();
 }
 
 // Check if randomNumber is set and is a valid integer
-if (isset($_POST['water_level_value']) && filter_var($_POST['water_level_value'], FILTER_VALIDATE_INT) && $_POST['turbidity_value'] && filter_var($_POST['turbidity_value'], FILTER_VALIDATE_INT) && $_POST['temperature_value'] && filter_var($_POST['temperature_value'], FILTER_VALIDATE_INT) &&$_POST['methane_value'] && filter_var($_POST['methane_value'], FILTER_VALIDATE_INT) &&!== false) {
+if (isset($_POST['water_level_value']) && filter_var($_POST['water_level_value'], FILTER_VALIDATE_INT) !== false && 
+    isset($_POST['turbidity_value']) && filter_var($_POST['turbidity_value'], FILTER_VALIDATE_INT) !== false && 
+    isset($_POST['temperature_value']) && filter_var($_POST['temperature_value'], FILTER_VALIDATE_INT) !== false && 
+    isset($_POST['methane_value']) && filter_var($_POST['methane_value'], FILTER_VALIDATE_INT) !== false) {
     $water_level = $_POST['water_level_value'];
     $turbidity = $_POST['turbidity_value'];
     $temperature = $_POST['temperature_value'];
@@ -40,63 +54,62 @@ if (isset($_POST['water_level_value']) && filter_var($_POST['water_level_value']
     }
 } else {
     echo "Invalid input";
+    exit();
 }
-    $turbidity_stmt = $conn->prepare("INSERT INTO Turbidity (value) VALUES (?);");
-    if ($turbidity_stmt) {
-        $turbidity_stmt->bind_param("i", $turbidity);
 
-        // Execute the statement
-        if ($turbidity_stmt->execute()) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $turbidity_stmt->error;
-        }
+$turbidity_stmt = $conn->prepare("INSERT INTO Turbidity (value) VALUES (?);");
+if ($turbidity_stmt) {
+    $turbidity_stmt->bind_param("i", $turbidity);
 
-        // Close the statement
-        $turbidity_stmt->close();
+    // Execute the statement
+    if ($turbidity_stmt->execute()) {
+        echo "New record created successfully";
     } else {
-        echo "Error: " . $conn->error;
-    } else{
-        echo "Invalid input";
+        echo "Error: " . $turbidity_stmt->error;
     }
 
-    $temperature_stmt = $conn->prepare("INSERT INTO Temperature (value) VALUES (?);");
-    if ($temperature_stmt) {
-        $temperature_stmt->bind_param("i", $temperature);
+    // Close the statement
+    $turbidity_stmt->close();
+} else {
+    echo "Error: " . $conn->error;
+    exit();
+}
 
-        // Execute the statement
-        if ($temperature_stmt->execute()) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $temperature_stmt->error;
-        }
+$temperature_stmt = $conn->prepare("INSERT INTO Temperature (value) VALUES (?);");
+if ($temperature_stmt) {
+    $temperature_stmt->bind_param("i", $temperature);
 
-        // Close the statement
-        $temperature_stmt->close();
+    // Execute the statement
+    if ($temperature_stmt->execute()) {
+        echo "New record created successfully";
     } else {
-        echo "Error: " . $conn->error;
-    } else{
-        echo "Invalid input";
+        echo "Error: " . $temperature_stmt->error;
     }
 
-    $methane_stmt = $conn->prepare("INSERT INTO Methane (value) VALUES (?);");
-    if ($methane_stmt) {
-        $methane_stmt->bind_param("i", $methane);
+    // Close the statement
+    $temperature_stmt->close();
+} else {
+    echo "Error: " . $conn->error;
+    exit();
+}
 
-        // Execute the statement
-        if ($methane_stmt->execute()) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $methane_stmt->error;
-        }
+$methane_stmt = $conn->prepare("INSERT INTO Methane (value) VALUES (?);");
+if ($methane_stmt) {
+    $methane_stmt->bind_param("i", $methane);
 
-        // Close the statement
-        $methane_stmt->close();
+    // Execute the statement
+    if ($methane_stmt->execute()) {
+        echo "New record created successfully";
     } else {
-        echo "Error: " . $conn->error;
-    } else{
-        echo "Invalid input";
+        echo "Error: " . $methane_stmt->error;
     }
+
+    // Close the statement
+    $methane_stmt->close();
+} else {
+    echo "Error: " . $conn->error;
+    exit();
+}
 // Close the connection
 $conn->close();
 ?>
